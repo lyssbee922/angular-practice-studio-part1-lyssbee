dec332276cba366c420f5a372fe9bd742a7c4b80
Elyssa Bezner <elyssabezner@MacBook-Pro.local>
2023-05-03 18:07:09 -0500

    all done

---
+++ src/app/experiment/experiment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiment',
+  templateUrl: './experiment.component.html',
+  styleUrls: ['./experiment.component.css']
+})
+export class ExperimentComponent {
+
+}
